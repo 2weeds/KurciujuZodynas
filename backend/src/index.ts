be2ad2c77b9387c70ef7b2d603a3b058644100c0
Mantas Svetlauskas <svettlauskas@gmail.com>
2022-03-10 17:08:15 +0200

    [FEAT] BE Port 8000, build added to gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 
-app.listen(8000, () => console.log("Listening to app at 3000"));
+app.listen(8000, () => console.log("Listening to app at 8000"));
+app.use(cors());
 app.use(express.static('src'));
 app.use(express.json());
-app.use(cors());
 
 app.get('/', (req, resp) => {
     resp.send("Hello world");
