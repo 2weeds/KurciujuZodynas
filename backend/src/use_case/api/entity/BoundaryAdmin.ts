78c9a3a059628a2e09ead7b3b77dc9c8f56ea611
Mantas Svetlauskas <svettlauskas@gmail.com>
2022-04-03 15:03:33 +0300

    [FEAT File cleanup done

--- backend/src/use_case/api/entity/BoundaryAdmin.ts
+++
@@ -1,7 +0,0 @@
-export class BoundaryAdmin {
-    readonly token: string;
-
-    constructor(token: string) {
-        this.token = token;
-    }
-}
--- frontend/src/RouteConstants.ts
+++ frontend/src/RouteConstants.ts
@@ -1,3 +1,2 @@
-export const ADMIN_AUTHORIZATION_PATH = "/system-management";
 export const LEXICON_UNITS_PATH = "/lexicon-units";
 export const PHRASES_PATH = "/phrases";
