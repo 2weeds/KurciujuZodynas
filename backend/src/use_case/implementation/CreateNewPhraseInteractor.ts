f667bdd2e2c4eb5a915299bd7201a89262157f1d
2weeds <dovydas.zamas@gmail.com>
2022-05-21 15:42:12 +0300

    [DATA]

--- backend/src/use_case/implementation/CreateNewPhraseInteractor.ts
+++ backend/src/use_case/implementation/CreateNewPhraseInteractor.ts
@@ -9,6 +9,12 @@ export class CreateNewPhraseInteractor implements CreateNewPhraseUseCase {
     }
 
     create(phrase: string, file: any): void {
+<<<<<<< Updated upstream
         this.gateway.createPhrase(phrase, file);
+=======
+        const formattedPhrase = phrase[0].toUpperCase() + phrase.slice(1, phrase.length).toLowerCase();
+        console.log(formattedPhrase);
+        this.gateway.createPhrase(formattedPhrase, file);
+>>>>>>> Stashed changes
     }
 }
