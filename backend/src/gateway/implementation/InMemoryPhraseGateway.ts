2104e8508c3830a3e9d51ecde912a05f37649bb9
2weeds <dovydas.zamas@gmail.com>
2022-05-23 05:25:09 +0300

    [FIX]

--- backend/src/gateway/implementation/InMemoryPhraseGateway.ts
+++ backend/src/gateway/implementation/InMemoryPhraseGateway.ts
@@ -25,11 +25,6 @@ export class InMemoryPhraseGateway implements PhraseGateway {
         // });
     }
     
-
-    constructor(filename: string) {
-        this.filename = filename;
-    }
-
     sendToExport(phrasesArray: Phrase[]): void {
         try {
             this.fs.writeFileSync('LexiconUnitsToExport.json', JSON.stringify(phrasesArray));
